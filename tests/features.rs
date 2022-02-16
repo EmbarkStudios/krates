0800f737993c3a817f1cc40134334007ae9060c7
Jake Shadle <jake.shadle@embark-studios.com>
2022-02-16 08:08:20 +0100

    Fix for mutliple features

---
+++ tests/features.rs
@@ -0,0 +1,13 @@
+#[test]
+fn multiple_features() {
+    let mut cmd = krates::Cmd::new();
+    cmd.manifest_path("tests/features/Cargo.toml").features(
+        ["blocking", "json", "multipart", "stream"]
+            .into_iter()
+            .map(|f| f.to_owned()),
+    );
+
+    let mdc: krates::cm::MetadataCommand = cmd.into();
+
+    mdc.exec().unwrap();
+}
